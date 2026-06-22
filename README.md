a09c2486c8c8fb5ebcd9fec25397706b10cc477b
alroychiang <alroychiang@outlook.com>
2026-06-22 20:40:35 +0800

    Initial setup: download script and gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# URA Planning RAG
---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")/data/raw"
+curl -O https://www.ura.gov.sg/-/media/Corporate/Planning/Master-Plan/MP25WrittenStatement.pdf
+# add more URLs here as you go
